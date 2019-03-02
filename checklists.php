0a593a1091af6d811dd27b15ff6476137b1c603c
Steve Henty <stevehenty@gmail.com>
2019-03-02 18:15:43 +0100

    Bump copyright

--- checklists.php
+++ checklists.php
@@ -9,7 +9,7 @@ Author URI: https://gravityflow.io
 License: GPL-2.0+
 
 ------------------------------------------------------------------------
-Copyright 2016-2018 Steven Henty S.L.
+Copyright 2016-2019 Steven Henty S.L.
 
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License, version 2, as
